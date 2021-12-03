25f354d1af496b2dc61a284402abc2c78e0787e1
Lukas Wais <lukas.wais@outlook.de>
2021-12-03 20:12:09 +0100

    skeleton

---
+++ src/anagram/AnagramFinder.java
@@ -0,0 +1,19 @@
+package anagram;
+
+public class AnagramFinder {
+
+    public static void main(String[] args) {
+        /*
+            Write some tests here
+         */
+    }
+
+
+    public static boolean areAnagrams(String string1, String string2) {
+        return false;
+    }
+
+    /*
+        Add more private methods if needed
+     */
+}
